bad925f7f77d9c16202788c6c2027370d0d5c590
AKKatung159 <6434496423@student.chula.ac.th>
2022-03-31 23:38:20 +0700

    delete debug

--- GameLauncher.java
+++ GameLauncher.java
@@ -6,7 +6,5 @@ public class GameLauncher {
 
     public static void main(String[] args) {
         new GameLauncher();
-        System.out.println("Hello AK Katung");
-        System.out.println("Hello oOInFINiteOo ");
     }
 }
