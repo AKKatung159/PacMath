79c1a62864a8d337ac612ad9adbaee6bb291a551
AKKatung159 <6434496423@student.chula.ac.th>
2022-04-01 23:48:47 +0700

    Testing new logic

--- GameLauncher.java
+++ GameLauncher.java
@@ -1,10 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class GameLauncher {
-
-    public GameLauncher(){
-        new Window();
-    }
-
     public static void main(String[] args) {
-        new GameLauncher();
+        JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(512,720,512,720));
+        panel.setLayout(new GridLayout(0,1));
+
+        frame.add(panel,BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("PackMath");
+        frame.pack();
+        frame.setVisible(true);
+
     }
 }
