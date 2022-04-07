daecacb7f2dbaf6fe752a50c02e8f37aa3155564
AKKatung159 <6434496423@student.chula.ac.th>
2022-04-07 22:31:15 +0700

    comment

--- GameKeyListener.java
+++ GameKeyListener.java
@@ -19,22 +19,22 @@ public class GameKeyListener implements KeyListener {
         key =e.getKeyCode();
         System.out.println(key);
         if(key==37){//&&getPoint().x>80
-            getPoint().x-=15;
+            getPoint().x-=15;//เลื่อนซ้าย
         }
         else if(key==39){//&&getPoint().x<580
-            getPoint().x+=15;
+            getPoint().x+=15;//เลื่อขวา
 
         }
         else if(key==38){
-            getPoint().y-=15;
+            getPoint().y-=15;//เลื่อนขึ้น
         }
         else if(key==40){
-            getPoint().y+=15;
+            getPoint().y+=15;//เลื่อนลง
 
         }
         else if(key==45){
-            getSize().x-=10*5.6;
-            getSize().y-=10;
+            getSize().x-=10*5.6;//เครื่องหมายลบย่อขนาด
+            getSize().y-=10;//เครื่องหมายบวกเพิ่มขนาด
         }
         else if(key==61){
             getSize().x+=10*5.6;
